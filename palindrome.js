8ccbd0b0c89a58c5476e5501bb0df2882de0e71e
dawidl022 <domdaw7@gmail.com>
2021-08-04 10:40:41 +0100

    Complete objects chapter

--- palindrome.js
+++ palindrome.js
@@ -1,15 +1,53 @@
-// Reverses a string
-function reverse(string) {
-  return Array.from(string).reverse().join("");
+"use strict";
+
+// Adds `reverse` to all strings.
+String.prototype.reverse = function() {
+  return Array.from(this).reverse().join("");
 }
 
-// Returns true for a palindrome, false otherwise
+String.prototype.blank = function() {
+  return !!this.match(/^\s*$/);
+}
 
-function isPalindrome(string) {
-  processedString = string.toLowerCase();
-  return processedString === reverse(processedString);
+Array.prototype.last = function() {
+  return this.slice(-1)[0];
 }
 
 function emailParts(emailAddress) {
   return emailAddress.toLowerCase().split("@");
 }
+
+// Defines a phrase object
+function Phrase(content) {
+  this.content = content;
+
+  this.processedContent = function() {
+    return this.processor(this.content);
+  } 
+
+  // Returns true for a palindrome, false otherwise
+  this.palindrome = function palindrome() {
+    return this.processedContent() === this.processedContent().reverse();
+  }
+
+  this.louder = function () {
+    return this.content.toUpperCase();
+  }
+
+  this.processor = function(string) {
+    return string.toLowerCase();
+  }
+}
+
+function TranslatedPhrase(content, translation) {
+  this.content = content;
+  this.translation = translation;
+
+  this.processedContent = function() {
+    return this.process(this.translation);
+  }
+}
+
+TranslatedPhrase.prototype = new Phrase();
+
+let frase = new TranslatedPhrase("recognise", "reconocer");
