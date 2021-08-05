0d7164aecd0c9ad295178f9a1ae0814ef03c8120
dawidl022 <domdaw7@gmail.com>
2021-08-05 13:47:47 +0100

    Add palindrome detector to web app

---
+++ bundle.js
@@ -0,0 +1,53 @@
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+let Phrase = require("dawidl022-palindrome");
+
+let string = prompt("Please enter a string for palindrome testing:");
+let phrase = new Phrase(string);
+
+if (phrase.palindrome()) {
+  alert(`"${phrase.content}" is a palindrome!`)
+} else {
+  alert(`"${phrase.content}" is not a palindrome.`)
+}
+
+},{"dawidl022-palindrome":2}],2:[function(require,module,exports){
+"use strict";
+module.exports = Phrase;
+
+// Adds `reverse` to all strings.
+String.prototype.reverse = function() {
+  return Array.from(this).reverse().join("");
+}
+
+String.prototype.blank = function() {
+  return !!this.match(/^\s*$/);
+}
+
+Array.prototype.last = function() {
+  return this.slice(-1)[0];
+}
+
+// Defines a phrase object
+function Phrase(content) {
+  this.content = content;
+
+  this.processContent = function() {
+    return this.letters().toLowerCase();
+  } 
+
+  // Returns true for a palindrome, false otherwise
+  this.palindrome = function palindrome() {
+    let processedContent = this.processContent();
+    return processedContent === processedContent.reverse();
+  }
+
+  this.louder = function () {
+    return this.content.toUpperCase();
+  }
+
+  this.letters = function(string = this.content) {
+    return (string.match(/[a-z]+/ig) ?? []).join("");
+  }
+}
+
+},{}]},{},[1]);
--- day.js
+++ day.js
@@ -1,6 +1,6 @@
 function dayName(date) {
   const daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday",
-                       "Thursday, Friday, Saturday"];
+                       "Thursday", "Friday", "Saturday"];
   return daysOfTheWeek[date.getDay()];
 }
 
