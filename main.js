44fe86f1866e92ece5255e36196618b6e3df0021
dawidl022 <domdaw7@gmail.com>
2021-08-05 14:14:41 +0100

    Bind pallindrome prompt to button

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 let Phrase = require("dawidl022-palindrome");
 
-let string = prompt("Please enter a string for palindrome testing:");
-let phrase = new Phrase(string);
+function palindromeTester() {
+  let string = prompt("Please enter a string for palindrome testing:");
 
-if (phrase.palindrome()) {
-  alert(`"${phrase.content}" is a palindrome!`)
-} else {
-  alert(`"${phrase.content}" is not a palindrome.`)
+  if (string === null) {
+    return;
+  } 
+  let phrase = new Phrase(string);
+  
+  if (phrase.palindrome()) {
+    alert(`"${phrase.content}" is a palindrome!`);
+  } else {
+    alert(`"${phrase.content}" is not a palindrome.`);
+  }
 }
+
+const btn = document.querySelector("#palindrome-tester");
+btn.addEventListener("click", palindromeTester);
+
